627323933f561f781c6931a87779a3c6cee3c0d0
richzjc <richzjc130109@gmail.com>
2025-05-18 12:13:21 +0800

    add files

---
+++ app/src/main/java/cn/richzjc/gittest/ad.java
@@ -0,0 +1,9 @@
+package cn.richzjc.gittest;
+
+public class ad {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("hello world");
+    }
+}
